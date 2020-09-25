99687bca61adef643a47559dc666b21c28f6813a
3103ski <bjastski@gmail.com>
2020-09-24 22:48:25 -0700

    adjustments to navigation and housekeeping html/scss

--- js/customJs.js
+++ js/customJs.js
@@ -1,14 +1,17 @@
 // changes background for nav when scrolled
 $(window).scroll(function () {
 	$('.fixed-res').toggleClass('scrolled', $(this).scrollTop() > 50);
+	$('.navbar-toggler').toggleClass('scrolled', $(this).scrollTop() > 50);
 });
 
+// menu page
 $(function () {
 	$('.see-appetizer').click(function () {
 		$('.appetizer-menu').toggleClass('show-menu');
 		$('.see-brunch').toggleClass('selector-hide-right');
 		$('.see-dinner').toggleClass('selector-hide-right');
 		$('.see-drinks').toggleClass('selector-hide-right');
+		$('.see-appetizer').toggleClass('active-pic');
 		$('.app-back').toggleClass('show-back');
 	});
 });
@@ -19,6 +22,7 @@ $(function () {
 		$('.see-appetizer').toggleClass('selector-hide-left');
 		$('.see-dinner').toggleClass('selector-hide-right');
 		$('.see-drinks').toggleClass('selector-hide-right');
+		$('.see-brunch').toggleClass('active-pic');
 		$('.brunch-back').toggleClass('show-back');
 	});
 });
@@ -29,6 +33,7 @@ $(function () {
 		$('.see-appetizer').toggleClass('selector-hide-left');
 		$('.see-brunch').toggleClass('selector-hide-left');
 		$('.see-drinks').toggleClass('selector-hide-right');
+		$('.see-dinner').toggleClass('active-pic');
 		$('.dinner-back').toggleClass('show-back');
 	});
 });
@@ -39,6 +44,15 @@ $(function () {
 		$('.see-appetizer').toggleClass('selector-hide-left');
 		$('.see-brunch').toggleClass('selector-hide-left');
 		$('.see-dinner').toggleClass('selector-hide-left');
+		$('.see-drinks').toggleClass('active-pic');
 		$('.drinks-back').toggleClass('show-back');
+	});
+});
+
+// about page
+$(function () {
+	$('.swap-about').click(function () {
+		$('.about-content').toggleClass('move-card');
+		$('.about-pic').toggleClass('move-pic');
 	});
 });
